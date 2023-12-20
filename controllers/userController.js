fe91f292bba62d24630414791d7042ff23d8ac6a
Prashant Patki <prashantpatki@Prashants-MacBook-Air.local>
2023-12-20 11:32:37 +0530

    did small changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
 const { mongo, default: mongoose } = require("mongoose");
-const ObjectId = require('mongoose').Types.ObjectId
-//@desc Get all contacts
-//@route GET /api/contacts
-//@access private
+const ObjectId = require("mongoose").Types.ObjectId;
+
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find();
   res.status(200).json(users);
 });
 
-//@desc Create New contact
-//@route POST /api/contacts
-//@access private
 const createUser = asyncHandler(async (req, res) => {
   console.log("The request body is :", req.body);
   const { username, age, hobbies } = req.body;
@@ -24,7 +19,6 @@ const createUser = asyncHandler(async (req, res) => {
     username,
     age,
     hobbies,
-    
   });
 
   res.status(201).json(user);
@@ -34,27 +28,23 @@ const createUser = asyncHandler(async (req, res) => {
 //@route GET /api/contacts/:id
 //@access private
 const getUser = asyncHandler(async (req, res) => {
-  if(mongoose.isValidObjectId(String(req.params.id)) === false){
+  if (mongoose.isValidObjectId(String(req.params.id)) === false) {
     res.status(400);
-    throw new Error("Invalid ID")
+    throw new Error("Invalid ID");
   }
   const user = await User.findById(req.params.id);
   if (!user) {
     res.status(404);
     throw new Error("user not found");
   }
-  
-  
+
   res.status(200).json(user);
 });
 
-//@desc Update contact
-//@route PUT /api/contacts/:id
-//@access private
 const updateUser = asyncHandler(async (req, res) => {
-  if(mongoose.isValidObjectId(String(req.params.id)) === false){
+  if (mongoose.isValidObjectId(String(req.params.id)) === false) {
     res.status(400);
-    throw new Error("Invalid ID")
+    throw new Error("Invalid ID");
   }
   const user = await User.findById(req.params.id);
   if (!user) {
@@ -62,33 +52,20 @@ const updateUser = asyncHandler(async (req, res) => {
     throw new Error("user not found");
   }
 
-  // if (contact.user_id.toString() !== req.user.id) {
-  //   res.status(403);
-  //   throw new Error("User don't have permission to update other user contacts");
-  // }
-
-  const updatedUser = await User.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
 
   res.status(200).json(updatedUser);
 });
 
-//@desc Delete contact
-//@route DELETE /api/contacts/:id
-//@access private
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (!user) {
     res.status(404);
     throw new Error("user not found");
   }
-  // if (contact.user_id.toString() !== req.user.id) {
-  //   res.status(403);
-  //   throw new Error("User don't have permission to update other user contacts");
-  // }
+
   await User.deleteOne({ _id: req.params.id });
   res.status(200).json(user);
 });
